7037e5d1107d10da64e8edea247d8ecc08593558
Joao Pedro <joaopedropavanelo@gmail.com>
2026-03-28 21:37:08 -0300

    criei uma classe JPA medico e uma Endereco que eu passei todos os campos que ja tinham , mas dessa vez eu passei eles como priate String e usando o modelo private .

---
+++ projeto.spring/src/main/java/br/joaopedro/projeto/spring/medico/Medico.java
@@ -0,0 +1,14 @@
+package br.joaopedro.projeto.spring.medico;
+
+import br.joaopedro.projeto.spring.endereco.Endereco;
+
+public class Medico {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    private Especialidade especialidade;
+    private Endereco endereco;
+
+}

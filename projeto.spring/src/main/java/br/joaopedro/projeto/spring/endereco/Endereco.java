46644268f1656e3aa57f064f6f23d6756f6a4675
Joao Pedro <joaopedropavanelo@gmail.com>
2026-03-30 22:26:07 -0300

    #4

--- projeto.spring/src/main/java/br/joaopedro/projeto/spring/endereco/Endereco.java
+++ projeto.spring/src/main/java/br/joaopedro/projeto/spring/endereco/Endereco.java
@@ -1,5 +1,14 @@
 package br.joaopedro.projeto.spring.endereco;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
 
     private String logradouro;

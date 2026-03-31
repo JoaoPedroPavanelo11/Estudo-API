f782b609e5709e3e255565c0e97bcfe20c8380ba
Joao Pedro <joaopedropavanelo@gmail.com>
2026-03-30 22:49:29 -0300

    #5

--- projeto.spring/src/main/java/br/joaopedro/projeto/spring/controller/MedicoController.java
+++ projeto.spring/src/main/java/br/joaopedro/projeto/spring/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package br.joaopedro.projeto.spring.controller;
 
+import br.joaopedro.projeto.spring.endereco.Endereco;
 import br.joaopedro.projeto.spring.medico.DadosCadastroMedicos;
+import br.joaopedro.projeto.spring.medico.Medico;
+import br.joaopedro.projeto.spring.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,8 +12,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("medico")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedicos dados){
-        System.out.println(dados);
+        repository.save(new Medico(dados ));
     }
 }
--- projeto.spring/src/main/java/br/joaopedro/projeto/spring/endereco/Endereco.java
+++ projeto.spring/src/main/java/br/joaopedro/projeto/spring/endereco/Endereco.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Endereco {
 
+
     private String logradouro;
     private String bairro;
     private String cep;
@@ -19,4 +20,14 @@ public class Endereco {
     private String cidade;
     private String uf;
 
+    public Endereco(DadosEndereco dadosEndereco) {
+        this.logradouro = dadosEndereco.logradouro();
+        this.bairro = dadosEndereco.bairro();
+        this.cep = dadosEndereco.cep();
+        this.numero = dadosEndereco.numero();
+        this.complemento = dadosEndereco.complemento();
+        this.cidade = dadosEndereco.cidade();
+        this.uf = dadosEndereco.uf();
+
+    }
 }
